8ce3691e15d24331cbbdd8306f13ba3d8b4c7b76
rambip <apero1808@gmail.com>
2022-11-03 10:47:04 +0000

    pretty printing

--- string_stack.py
+++ string_stack.py
@@ -28,7 +28,7 @@ class StringStack(list):
     def __str__(self) -> str:
         """Render the stack as a string."""
         # return "[\t"+"\n\t".join(self) + "\n]"
-        res = " ┏\n ┃"
-        res += "\n ┃".join(self)
+        res = " ┏\n ┃  "
+        res += "\n ┃  ".join(self)
         res += "\n ┗"
         return res
