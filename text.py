bbb4458821ad2014b47484fccdb67e31e3fc93f1
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-11-06 02:09:17 +0100

    doc of the wrap function

--- text.py
+++ text.py
@@ -14,7 +14,17 @@ def height(txt: list[str]) -> int:
 
 
 def wrap(txt: list[str], max_width: int) -> list[str]:
-    """wrap a text to limit idx width."""
+    """wrap a text to limit its width.
+    Args:
+        txt (list[str]): The text to wrap.
+        max_width (int): The width that you wrap at.
+            Lines shorter won't be touched
+            lines longer will be split in lines of this length (or less for the
+            last one).
+    Returns:
+        list[str]: The text, but split into lines of length less or equal to
+            `max_width`.
+    """
     res = []
     for line in txt:
         if len(line) < max_width:  # nothing to wrap
