fc13fc4c6e012880a16d5db1bfd66cd5baa33f4f
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-11-06 02:05:18 +0100

    function to wrap text at a certain width

--- text.py
+++ text.py
@@ -13,6 +13,17 @@ def height(txt: list[str]) -> int:
     return len(txt)
 
 
-
+def wrap(txt: list[str], max_width: int) -> list[str]:
+    """wrap a text to limit idx width."""
+    res = []
+    for line in txt:
+        if len(line) < max_width:  # nothing to wrap
+            res.append(line)
+        else:
+            # (len(line) // max_width) is the number of lines you'll get
+            # splitting the current one
+            for i in range(len(line) // max_width):
+                res.append(line[max_width*i:max_width*(i+1)])
+    return res
 
 
