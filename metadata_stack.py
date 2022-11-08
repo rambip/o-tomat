611f4aca87c4844bc06b4b23444ab5250500724f
Oscar Plaisant <76111415+OsKaR31415@users.noreply.github.com>
2022-11-08 23:42:55 +0100

    create a metadata stack class (MetaStack)

--- metadata_stack.py
+++ metadata_stack.py
@@ -1,1 +1,22 @@
-class MetaStack(list):
+from stack import Stack
+
+# TODO: implement what is metadata
+class MetaData:
+    def __init__(self):
+        pass
+
+class MetaStack(Stack):
+    @property
+    def stack_contents_type(self):
+        return MetaData
+
+    @property
+    def bottom_of_pile_symbol(self):
+        return None
+
+    def render(self) -> [str]:
+        # TODO: render or __str__
+
+
+
+
